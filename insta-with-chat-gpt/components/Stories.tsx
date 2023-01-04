8c57c68e3067f98fae60684c6060397f9f13e128
Shaohao Lin <shaohlin@microsoft.com>
2023-01-03 17:01:56 -0800

    facker with story

--- insta-with-chat-gpt/components/Stories.tsx
+++ insta-with-chat-gpt/components/Stories.tsx
@@ -1,19 +1,35 @@
 import { faker } from "@faker-js/faker";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Story from "./Story";
 
 function Stories() {
+  const [suggestions, setSuggestions] = useState<any>([]);
+
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
-      ...faker.helpers.contextualCard(),
+      userId: faker.datatype.uuid(),
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password(),
+      birthday: faker.date.birthdate(),
+      registeredAt: faker.date.past(),
       id: i,
     }));
 
     console.log(suggestions);
+    setSuggestions(suggestions);
   }, []);
 
   return (
     <div>
-      <h1>Stories</h1>
+      {suggestions.map((profile: any) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
     </div>
   );
 }
