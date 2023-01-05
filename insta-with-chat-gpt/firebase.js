0e17e66ddcd3d3c1be410069f60c4150cba5c331
Shaohao Lin <shaohlin@microsoft.com>
2023-01-05 11:59:14 -0800

    set up firebase connection

---
+++ insta-with-chat-gpt/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHYjeDcf3pGudqdjGAYjAo3yOYC96J9jg",
+  authDomain: "insta-with-chat-gpt.firebaseapp.com",
+  projectId: "insta-with-chat-gpt",
+  storageBucket: "insta-with-chat-gpt.appspot.com",
+  messagingSenderId: "280188427070",
+  appId: "1:280188427070:web:ca1be79033aa2be08baa6f",
+  measurementId: "G-2SLCC5LJWK",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const analytics = getAnalytics(app);
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
---
+++ insta-with-chat-gpt/pages/auth/signin.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function signIn() {
+  return <div>I am the sign in page.</div>;
+}
+
+export default signIn;
