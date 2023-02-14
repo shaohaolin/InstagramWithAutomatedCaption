3d8441bf1c43b06599afe30c47426ecaac5cd350
Shaohao Lin <shaohlin@microsoft.com>
2023-02-14 15:27:40 -0800

    fix render user image in mini profile

--- insta-with-chat-gpt/components/MiniProfile.tsx
+++ insta-with-chat-gpt/components/MiniProfile.tsx
@@ -1,13 +1,12 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 function MiniProfile() {
   const { data: session } = useSession();
-  console.log(session);
   return (
     <div className="flex items-center justify-between mt-14 ml-10">
       <img
         className="rounded-full border p-[2px] w-16 h-16"
-        src="https://i.pinimg.com/236x/d9/97/4c/d9974c818ebf4e514deceb4ce67fdfb0.jpg"
+        src={session?.user?.image as unknown as undefined}
         alt=""
       />
 
